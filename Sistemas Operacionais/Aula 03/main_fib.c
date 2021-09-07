20504797f2c7aa4772775c1686b1c4187027df1b
Antonio Muniz <antonio.gustavo_@live.com>
2021-09-07 12:44:15 -0300

    Fixed threads exercises

--- Sistemas Operacionais/Aula 03/main_fib.c
+++ Sistemas Operacionais/Aula 03/main_fib.c
@@ -6,10 +6,8 @@
 
 int fib(int n)                             
   {                                          
-    if (n == 1) return 1;                   
-    else                                    
-      if (n == 2) return 1;                 
-      else return fib(n - 1) + fib(n - 2);                             
+    if (n <= 1) return n;                                 
+    else return fib(n - 1) + fib(n - 2);                             
 }   
 
 int main() {
